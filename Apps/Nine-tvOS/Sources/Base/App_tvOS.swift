7d5566e304bdafd90ca19390edd6c8f5b706bccb
Robtles <robbeyroad@gmail.com>
2024-05-07 11:34:53 +0200

    Added tvOS target

---
+++ Apps/Nine-tvOS/Sources/Base/App_tvOS.swift
@@ -0,0 +1,17 @@
+//
+//  App_tvOS.swift
+//  Nine-tvOS
+//
+//  Created by Rob on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct App_tvOS: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
