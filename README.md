4a9126e8ddf6f5efb28222db244ac6dd071ddee4
Robtles <robbeyroad@gmail.com>
2024-05-07 10:05:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Nine
+
+This app corresponds to the Nine card game, expected to work on all Apple platforms: iOS, macOS, tvOS, watchOS and visionOS.
---
+++ Sources/Base/NineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NineApp.swift
+//  Nine
+//
+//  Created by Rob on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
