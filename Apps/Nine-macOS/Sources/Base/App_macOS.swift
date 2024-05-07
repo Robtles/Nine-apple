f4b2ea7b232bcf83a34422c355a88c8abd42dbbc
Robtles <robbeyroad@gmail.com>
2024-05-07 11:27:02 +0200

    Added macOS target

---
+++ Apps/Nine-macOS/Sources/Base/App_macOS.swift
@@ -0,0 +1,17 @@
+//
+//  App_macOS.swift
+//  Nine-macOS
+//
+//  Created by Rob on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct App_macOS: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
