01ef1a3743b9fc5e68a41d94f6e27dc4fe9b3bf8
Robtles <robbeyroad@gmail.com>
2024-05-07 11:38:24 +0200

    Added visionOS target

---
+++ Apps/Nine-visionOS/Sources/Base/App_visionOS.swift
@@ -0,0 +1,17 @@
+//
+//  App_visionOS.swift
+//  Nine-visionOS
+//
+//  Created by Rob on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct App_visionOS: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
