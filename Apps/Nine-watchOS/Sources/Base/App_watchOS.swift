70fa2653df4eaac6f0f843dcefc41b4e2fb8d35d
Robtles <robbeyroad@gmail.com>
2024-05-07 11:32:31 +0200

    Added watchOS target

---
+++ Apps/Nine-watchOS/Sources/Base/App_watchOS.swift
@@ -0,0 +1,17 @@
+//
+//  App_watchOS.swift
+//  Nine-watchOS Watch App
+//
+//  Created by Rob on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct App_watchOS: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
